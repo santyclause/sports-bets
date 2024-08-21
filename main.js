208cc444f4eec9c19a8d11835d6b58549ee0f400
Matthew Marchal <matthew.marchal@gmail.com>
2024-08-21 14:47:35 -0600

    created player roster

---
+++ main.js
@@ -0,0 +1,25 @@
+let bank = 100;
+
+const players = [
+  { team: 'red', player: '🐀', skill: 100, name: 'Lurk' },
+  { team: 'red', player: '🐺', skill: 99, name: 'Milo' },
+  { team: 'red', player: '🦊', skill: 88, name: 'Kit' },
+  { team: 'red', player: '🐷', skill: 27, name: 'Oinkington' },
+  { team: 'red', player: '🐮', skill: 10, name: 'Bessie' },
+  { team: 'red', player: '🐵', skill: 71, name: 'Steve' },
+  { team: 'red', player: '🐻', skill: 64, name: 'Smokey' },
+  { team: 'red', player: '🦝', skill: 42, name: 'Tanner' },
+  { team: 'red', player: '🐹', skill: 1, name: 'Hammy' },
+  { team: 'red', player: '🐭', skill: 75, name: 'Mannfred' },
+  { team: 'blue', player: '🐴', skill: 76, name: 'Stanley' },
+  { team: 'blue', player: '🐱', skill: 50, name: 'Thunder' },
+  { team: 'blue', player: '🐯', skill: 82, name: 'Mr. Whiskers' },
+  { team: 'blue', player: '🦁', skill: 73, name: 'Simba' },
+  { team: 'blue', player: '🐰', skill: 51, name: 'Rapscallion' },
+  { team: 'blue', player: '🐻‍❄️', skill: 77, name: 'Barry' },
+  { team: 'blue', player: '🐼', skill: 31, name: 'Po' },
+  { team: 'blue', player: '🐸', skill: 5, name: 'Kermit' },
+  { team: 'blue', player: '🐨', skill: 19, name: 'Dropbear' },
+  { team: 'blue', player: '🦄', skill: 98, name: 'Celestia' }
+]
+
